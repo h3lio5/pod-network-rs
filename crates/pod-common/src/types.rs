cf0303bb82ebf449b1001b46166ffbb04db7ef9d
h3lio5 <akashsm@ce.iitr.ac.in>
2025-04-03 22:17:42 +0530

    remove unused types

--- crates/pod-common/src/types.rs
+++ crates/pod-common/src/types.rs
@@ -1,9 +1,6 @@
 use bincode::{Decode, Encode};
-use ed25519_dalek::VerifyingKey as PublicKey;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
-
-use crate::PodError;
 
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, Hash, Encode, Decode)]
 pub struct Transaction {
@@ -13,44 +10,6 @@ pub struct Transaction {
 }
 
 pub type TransactionId = [u8; 32];
-
-#[derive(Clone, Debug)]
-pub enum PeerId {
-    ReplicaId(PublicKey),
-    ClientId(PublicKey),
-}
-
-impl PeerId {
-    pub fn from_replica_id_bytes(bytes: &[u8]) -> Result<PeerId, PodError> {
-        let replica_id = Self::raw_replica_pubkey_from_bytes(bytes)?;
-        Ok(PeerId::ReplicaId(replica_id))
-    }
-
-    pub fn raw_replica_pubkey_from_bytes(bytes: &[u8]) -> Result<PublicKey, PodError> {
-        let replica_id = PublicKey::from_bytes(&bytes.try_into().map_err(|_| {
-            PodError::SerializationError("Failed to deserialize public key".to_string())
-        })?)
-        .map_err(|_| {
-            PodError::SerializationError("Failed to deserialize public key".to_string())
-        })?;
-        Ok(replica_id)
-    }
-
-    pub fn from_client_id_bytes(bytes: &[u8]) -> Result<PeerId, PodError> {
-        let client_id = Self::raw_client_pubkey_from_bytes(bytes)?;
-        Ok(PeerId::ReplicaId(client_id))
-    }
-
-    pub fn raw_client_pubkey_from_bytes(bytes: &[u8]) -> Result<PublicKey, PodError> {
-        let client_id = PublicKey::from_bytes(&bytes.try_into().map_err(|_| {
-            PodError::SerializationError("Failed to deserialize public key".to_string())
-        })?)
-        .map_err(|_| {
-            PodError::SerializationError("Failed to deserialize public key".to_string())
-        })?;
-        Ok(client_id)
-    }
-}
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub enum TransactionStatus {
