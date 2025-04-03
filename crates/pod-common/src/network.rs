3d6066dde085f0dde4c00b062849209c9e298e6f
h3lio5 <akashsm@ce.iitr.ac.in>
2025-04-03 21:06:10 +0530

    define ws sink type and fix type issues

--- crates/pod-common/src/network.rs
+++ crates/pod-common/src/network.rs
@@ -1,107 +1,26 @@
 use crate::errors::PodError;
 use crate::types::Message;
 use async_trait::async_trait;
-use ed25519_dalek::VerifyingKey as PublicKey;
-use futures_util::{SinkExt, StreamExt};
-use std::collections::HashMap;
+use futures_util::{SinkExt, StreamExt, stream::{SplitSink, SplitStream}};
 use tokio::{net::TcpStream, sync::broadcast};
-use tokio_tungstenite::{tungstenite, WebSocketStream};
+use tokio_tungstenite::{tungstenite::{self, Message as TungMessage}, WebSocketStream};
 use tracing::warn;
 
+pub type WsSink = SplitSink<WebSocketStream<TcpStream>, TungMessage>;
+
+/// Core network functionality for message broadcasting and receiving
 #[async_trait]
 pub trait NetworkTrait: Send + Sync {
-    async fn send_to_replica(
-        &self,
-        replica_id: PublicKey,
-        message: Message,
-    ) -> Result<(), PodError>;
     async fn broadcast(&self, message: Message) -> Result<(), PodError>;
     fn subscribe(&self) -> broadcast::Receiver<Message>;
     async fn listen(&self, address: &str) -> Result<(), PodError>;
-    fn replicas(&self) -> Vec<PublicKey>;
 }
 
-pub struct Network {
-    tx: broadcast::Sender<Message>,
-    peers: HashMap<PublicKey, String>,
-}
-
-impl Network {
-    pub fn new(peers: HashMap<PublicKey, String>) -> Self {
-        let (tx, _) = broadcast::channel(1000);
-        Self { tx, peers }
-    }
-
-    async fn send_message(&self, addr: &str, message: Message) -> Result<(), PodError> {
-        let (mut ws_stream, _) = tokio_tungstenite::connect_async(addr)
-            .await
-            .map_err(|e| PodError::NetworkError(e.to_string()))?;
-        let data = bincode::encode_to_vec(&message, bincode::config::standard())
-            .map_err(|e| PodError::NetworkError(e.to_string()))?;
-        ws_stream
-            .send(tungstenite::protocol::Message::Binary(data.into()))
-            .await
-            .map_err(|e| PodError::NetworkError(e.to_string()))?;
-        Ok(())
-    }
-}
-
-#[async_trait]
-impl NetworkTrait for Network {
-    async fn send_to_replica(
-        &self,
-        replica_id: PublicKey,
-        message: Message,
-    ) -> Result<(), PodError> {
-        let addr = self
-            .peers
-            .get(&replica_id)
-            .ok_or_else(|| PodError::NetworkError("Unknown replica".to_string()))?;
-        self.send_message(addr, message).await
-    }
-
-    async fn broadcast(&self, message: Message) -> Result<(), PodError> {
-        for (_, addr) in self.peers.iter() {
-            if let Err(e) = self.send_message(addr, message.clone()).await {
-                warn!("Failed to send to {}: {}", addr, e);
-            }
-        }
-        Ok(())
-    }
-
-    fn subscribe(&self) -> broadcast::Receiver<Message> {
-        self.tx.subscribe()
-    }
-
-    async fn listen(&self, address: &str) -> Result<(), PodError> {
-        let listener = tokio::net::TcpListener::bind(address)
-            .await
-            .map_err(|e| PodError::NetworkError(e.to_string()))?;
-        let tx = self.tx.clone();
-        tokio::spawn(async move {
-            while let Ok((stream, _)) = listener.accept().await {
-                let tx = tx.clone();
-                tokio::spawn(async move {
-                    if let Ok(ws_stream) = tokio_tungstenite::accept_async(stream).await {
-                        handle_connection(ws_stream, tx).await;
-                    }
-                });
-            }
-        });
-        tracing::info!("WebSocket server listening on {}", address);
-        Ok(())
-    }
-
-    fn replicas(&self) -> Vec<PublicKey> {
-        self.peers.keys().cloned().collect()
-    }
-}
-
-async fn handle_connection(
-    mut ws_stream: WebSocketStream<TcpStream>,
+pub async fn handle_connection(
+    mut read: SplitStream<WebSocketStream<TcpStream>>,
     tx: broadcast::Sender<Message>,
 ) {
-    while let Some(Ok(msg)) = ws_stream.next().await {
+    while let Some(Ok(msg)) = read.next().await {
         if let tungstenite::Message::Binary(data) = msg {
             match bincode::decode_from_slice(&data, bincode::config::standard()) {
                 Ok((message, _)) => {
@@ -114,4 +33,16 @@ async fn handle_connection(
             }
         }
     }
+}
+
+pub async fn send_message_to_peer(
+    sink: &mut WsSink,
+    message: Message,
+) -> Result<(), PodError> {
+    let data = bincode::encode_to_vec(message, bincode::config::standard())
+        .map_err(|e| PodError::NetworkError(e.to_string()))?;
+    sink.send(tungstenite::protocol::Message::Binary(data.into()))
+        .await
+        .map_err(|e| PodError::NetworkError(e.to_string()))?;
+    Ok(())
 }
